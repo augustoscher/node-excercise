fb2c4d6a66087ad3748cbfad34b4f1d63cd42b09
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-09-05 01:39:17 -0300

    node excericises

--- process/ex08-process.js
+++ process/ex08-process.js
@@ -1,2 +1,12 @@
 //executar: node ex08-process.js --producao outro param
 console.log(process.argv);
+
+const temParam = (param) => {
+  return process.argv.indexOf(param) !== -1
+}
+
+if (temParam("--prod")) {
+  console.log("prod")
+} else {
+  console.log("not prod")
+}
