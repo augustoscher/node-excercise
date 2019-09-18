3d0f91ca5cedb7b9d980a119a9b623b122fd483d
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-09-18 00:23:04 -0300

    setting output directory and filename

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,5 +2,10 @@ const webpack = require('webpack');
 
 module.exports = {
   mode: 'production',
-  entry: './src/principal.js'
+  entry: './src/principal.js',
+  output: {
+    //set output directory and file name
+    filename: 'principal.js',
+    path: __dirname + '/public'
+  }
 }
