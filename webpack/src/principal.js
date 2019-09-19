f2471c7668e019543113851d095dbb471853ea1d
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-09-19 01:07:12 -0300

    css modules

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,5 +1,6 @@
 import Pessoa from './pessoa';
 import './modules/module-a';
+import './assets/css/estilo.css';
 
 const p = new Pessoa();
 console.log(p.cumprimentar());
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,20 @@
 const webpack = require('webpack');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/principal.js',
   output: {
     //set output directory and file name
     filename: 'principal.js',
     path: __dirname + '/public'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader', //adiciona CSS a DOM injetando a tag <style>
+        'css-loader' //interpreta @import, url()
+      ]
+    }]
   }
 }
