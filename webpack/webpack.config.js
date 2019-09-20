5166daeaca846729570648b271132388fa241e38
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-09-20 13:14:50 -0300

    cross-env e build para production e development

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,13 +1,25 @@
+const modoDev = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptmizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: modoDev ? 'development' : 'production',
   entry: './src/principal.js',
   output: {
     //set output directory and file name
     filename: 'principal.js',
     path: __dirname + '/public'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+      }),
+      new OptmizeCSSAssetsPlugin({})
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
