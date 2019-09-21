47edf9dd40f150640c56a4d3b5a8926201464914
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-09-21 14:12:00 -0300

    css grid and css flexbox

---
+++ css/webpack.config.js
@@ -0,0 +1,47 @@
+const modoDev = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptmizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  mode: modoDev ? 'development' : 'production',
+  entry: './src/app.js',
+  output: {
+    //set output directory and file name
+    filename: 'app.js',
+    path: __dirname + '/public'
+  },
+  devServer: {
+    contentBase: "./public",
+    port: 9000,
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+      }),
+      new OptmizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'estilo.css',
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.s?[ac]ss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        // 'style-loader', //adiciona CSS a DOM injetando a tag <style>
+        'css-loader', //interpreta @import, url()
+        'sass-loader'
+      ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: ['file-loader']
+    }]
+  }
+}
